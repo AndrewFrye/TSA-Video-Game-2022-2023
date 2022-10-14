ff768a9374bda69e0e7af583b9dd9a5c78f6b7c0
Andrew Frye <dewdew72805@gmail.com>
2022-10-13 21:43:27 -0400

    Addded basic gravity system, needs slopes to be properly handled, also updated gitignore

--- Assets/Src/PlayerMove.cs
+++ Assets/Src/PlayerMove.cs
@@ -12,6 +12,11 @@ public class PlayerMove : MonoBehaviour
 
     public Vector3 movement;
     public float speed;
+    public float distToGround;
+    public float gravity;
+
+    private bool grounded;
+    private bool jumping;
 
     private void Start()
     {
@@ -21,12 +26,31 @@ public class PlayerMove : MonoBehaviour
 
     public void Update()
     {
+        checkGround();
         //set the X and Z movement of the player based off the input
         movement = new Vector3(input.FindAction("Move").ReadValue<Vector2>().x, 0,
             input.FindAction("Move").ReadValue<Vector2>().y);
         
         movement *= speed * Time.deltaTime; //Normalize the speed before adding in the gravity component
 
+
+
         controller.Move(movement);
+    }
+
+    private void gravityCalc() //Needs slopes to be handled
+    {
+        if(!grounded && !jumping) movement.y = -gravity * Time.deltaTime;
+    }
+
+    private void checkGround()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, distToGround + 0.1f))
+        {
+            grounded = true;
+        } else
+        {
+            grounded = false;
+        }
     }
 }
